a50b4c9d33150bb99dbce79f4ac923d4558bd6fa
Isaac Rezey <isaacrez20@gmail.com>
2021-01-08 14:52:47 -0500

    Add CRUD methods w/o functionality to DAOs

--- src/main/java/com/sg/FinancialApp/data/UserDaoDB.java
+++ src/main/java/com/sg/FinancialApp/data/UserDaoDB.java
@@ -5,6 +5,8 @@
  */
 package com.sg.FinancialApp.data;
 
+import com.sg.FinancialApp.models.User;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -21,6 +23,31 @@ public class UserDaoDB implements UserDao {
     @Autowired
     public UserDaoDB(JdbcTemplate jdbcTemplate) {
         this.jdbc = jdbcTemplate;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User getUserById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User addUser() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateUser(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteUserById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
     
