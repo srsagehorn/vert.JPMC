03e85f9cf7d584ceefbf990d0d4e6b164cce2cf7
Isaac Rezey <isaacrez20@gmail.com>
2021-01-11 14:43:24 -0500

    Implement getUserById

--- src/main/java/com/sg/FinancialApp/data/UserDaoDB.java
+++ src/main/java/com/sg/FinancialApp/data/UserDaoDB.java
@@ -10,6 +10,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -40,7 +41,12 @@ public class UserDaoDB implements UserDao {
     // NOT TESTED
     @Override
     public User getUserById(String id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            final String GET_USER_BY_ID = "SELECT * FROM user WHERE userId = ?";
+            return jdbc.queryForObject(GET_USER_BY_ID, new UserMapper(), id);
+        } catch (DataAccessException e) {
+            return null;
+        }
     }
 
 
