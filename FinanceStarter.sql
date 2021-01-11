808bb7d03d6c99213158db4ee115eeea205e50ab
isaacrez <50759778+isaacrez@users.noreply.github.com>
2021-01-11 11:08:11 -0500

    Add "Quantity" property to "Request"

--- FinanceStarter.sql
+++ FinanceStarter.sql
@@ -7,6 +7,7 @@ use financeTest;
 create table request(
 	requestId int primary key auto_increment not null,
     reqTime datetime not null,
+    quantity dec(6,6) not null,
     stockCode varchar(4) not null,
     value dec(6,2) not null
 );
@@ -34,6 +35,7 @@ use finance;
 create table request(
 	requestId int primary key auto_increment not null,
     reqTime datetime not null,
+    quantity dec(6,6) not null,
     stockCode varchar(4) not null,
     value dec(6,2) not null
 );
