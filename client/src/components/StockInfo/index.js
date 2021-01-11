6a270ba8207aae7bef21252439923124a5420c83
MatiasTong <58445480+MatiasTong@users.noreply.github.com>
2021-01-11 10:32:10 -0500

    search feature edits

--- client/src/components/StockInfo/index.js
+++ client/src/components/StockInfo/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from 'react'
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
