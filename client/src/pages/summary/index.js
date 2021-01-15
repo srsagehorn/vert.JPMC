ff8284095202805b5ee7d77a5e2242dc290b441f
MatiasTong <58445480+MatiasTong@users.noreply.github.com>
2021-01-15 09:14:34 -0500

    changed background color of table on summary page

--- client/src/pages/summary/index.js
+++ client/src/pages/summary/index.js
@@ -5,19 +5,20 @@ import PortfolioTable from '../../components/PortfolioTable'
 import Card from "@material-ui/core/Card";
 // import CardMedia from "@material-ui/core/CardMedia";
 // import Paper from "@material-ui/core/Paper";
-// import { ThemeProvider } from "@material-ui/styles";
-// import theme from "../../themes/theme";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../../themes/theme";
 
 export default function SummaryPage() {
     return (
         <div>
             <h1 className = "center">Top Stocks To Watch</h1>
+              <ThemeProvider theme={theme}>
             <PortfolioTable />
+                         </ThemeProvider> 
             <News />
-              {/* <ThemeProvider theme={theme}>
 
            
-             <Grid container justify="center" spacing={3}>
+             {/* <Grid container justify="center" spacing={3}>
                  <Grid item lg={6} md={6} style={{ margin: "3rem" }} >
                      <Card style={{ padding: "1rem" }}>
                          <CardMedia
@@ -29,8 +30,7 @@ export default function SummaryPage() {
                          />
                      </Card>
                  </Grid>
-             </Grid>
-             </ThemeProvider> */}
+             </Grid> */}
          </div>
     )
 };
